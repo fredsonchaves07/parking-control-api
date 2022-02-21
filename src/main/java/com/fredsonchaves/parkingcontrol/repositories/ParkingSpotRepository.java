336ee6b6aa6b5a96bc5e0287796afec122071a44
Fredson Chaves <fredsonchaves07@gmail.com>
2022-02-21 16:05:07 -0300

    feat: add repositories, controller, services and post endpoint

---
+++ src/main/java/com/fredsonchaves/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,13 @@
+package com.fredsonchaves.parkingcontrol.repositories;
+
+import com.fredsonchaves.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+
+}
---
+++ src/main/java/com/fredsonchaves/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,22 @@
+package com.fredsonchaves.parkingcontrol.services;
+
+import com.fredsonchaves.parkingcontrol.models.ParkingSpotModel;
+import com.fredsonchaves.parkingcontrol.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+}
